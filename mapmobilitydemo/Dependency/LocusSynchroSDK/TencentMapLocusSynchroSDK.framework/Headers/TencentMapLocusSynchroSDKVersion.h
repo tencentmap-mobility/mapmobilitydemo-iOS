b18eb2755450ea32f24cd5af1c55ba1140ee963c
mol <mikedpzhang@tencent.com>
2021-10-25 16:03:37 +0800

    更新司乘同显基础库至v2.3.1修复签名校验问题

--- mapmobilitydemo/Dependency/LocusSynchroSDK/TencentMapLocusSynchroSDK.framework/Headers/TencentMapLocusSynchroSDKVersion.h
+++ mapmobilitydemo/Dependency/LocusSynchroSDK/TencentMapLocusSynchroSDK.framework/Headers/TencentMapLocusSynchroSDKVersion.h
@@ -12,7 +12,7 @@
 #ifndef TencentMapLocusSynchroSDKVersion_h
 #define TencentMapLocusSynchroSDKVersion_h
 
-#define TencentMapLocusSynchroSDKVersionNumber    020300
+#define TencentMapLocusSynchroSDKVersionNumber    020301
 
 // SDK版本
 FOUNDATION_EXTERN NSString * const TencentMapLocusSynchroSDKVersion;

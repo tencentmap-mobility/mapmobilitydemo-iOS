a5120678b741e92a23c40afd2a61ede8b18359c3
mol <mikedpzhang@tencent.com>
2021-12-13 15:37:31 +0800

    更新司乘同显司机端至2.4.1

--- mapmobilitydemo/Dependency/LocusSynchroSDK/TencentMapLocusSynchroDriverSDK.framework/Headers/TencentMapLocusSynchroDriverSDKVersion.h
+++ mapmobilitydemo/Dependency/LocusSynchroSDK/TencentMapLocusSynchroDriverSDK.framework/Headers/TencentMapLocusSynchroDriverSDKVersion.h
@@ -11,7 +11,7 @@
 #ifndef TencentMapLocusSynchroDriverSDKVersion_h
 #define TencentMapLocusSynchroDriverSDKVersion_h
 
-#define TencentMapLocusSynchroDriverSDKVersionNumber    020400
+#define TencentMapLocusSynchroDriverSDKVersionNumber    020401
 
 // SDK版本
 FOUNDATION_EXTERN NSString * const TencentMapLocusSynchroDriverSDKVersion;
